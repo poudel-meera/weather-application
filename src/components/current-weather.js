d9858611b3a29bc9a7a665191456b34ebe1c2782
poudel-meera <129344184+poudel-meera@users.noreply.github.com>
2023-04-10 14:59:06 -0400

    current weather shows air quality component

--- src/components/current-weather.js
+++ src/components/current-weather.js
@@ -1,64 +1,60 @@
 import React from "react";
+import AirQuality from "./air-quality";
 import "../app.css";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionItemHeading,
-  AccordionItemButton,
-  AccordionItemPanel,
-} from "react-accessible-accordion";
 
-import AirQuality from "./air-quality"; // import the AirQuality component
-
-const Forecast = ({ data }) => {
-  const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  const dayInWeek = new Date().getDay();
-  const forecastDays = weekdays.slice(dayInWeek, weekdays.length).concat(weekdays.slice(0, dayInWeek));
+const CurrentWeather = ({ data }) => {
+  // If there is no data yet, return null
+  if (!data) {
+    return null;
+  }
+  // Destruct the properties we need from the data object
+  const { city, weather, main, wind, clouds } = data;
 
   return (
-    <div>
-      <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, index) => (
-          <AccordionItem key={index}>
-            <AccordionItemHeading>
-              <AccordionItemButton>
-                <div className="daily-item">
-                  <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
-                  <label className="day">{forecastDays[index]}</label>
+    <section className="weather">
+      <div className="main-description">
+        <div>
+          <h2 className="city">{city}</h2>
+          <p className="weather-description">{weather[0].description}</p>
+        </div>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${weather[0].icon}.png`}
+        />
+      </div>
 
-                  {/* AirQuality component */}
-                  <label className="air-quality"> <AirQuality airQuality={item.airQuality}/> </label>
+      <div className="rest-of-the-descriptions">
+        <h3 className="temperature">{Math.round(main.temp)}°F</h3>
+        <div className="details">
+          <div className="row">
+            <span className="label">Humidity:</span>
+            <span className="value">{main.humidity}%</span>
+          </div>
+          <div className="row">
+            <span className="label">Wind:</span>
+            <span className="value">{wind.speed} mph</span>
+          </div>
+          <div className="row">
+            <span className="label">Clouds:</span>
+            <span className="value">{clouds.all}%</span>
+          </div>
+          <div className="row">
+            <span className="label">Feels Like:</span>
+            <span className="value">{Math.round(main.feels_like)}°F</span>
+          </div>
 
-                  <label className="description">{item.weather[0].description}</label>
-                  <label className="min-max">{Math.round(item.main.temp_max)}°F /{Math.round(item.main.temp_min)}°F</label>
-                </div>
-              </AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <div className="daily-details-grid">
-                <div className="daily-details-grid-item">
-                  <label>Humidity: </label>
-                  <label>{item.main.humidity}</label>
-                </div>
-                <div className="daily-details-grid-item">
-                  <label>Clouds:</label>
-                  <label>{item.clouds.all}%</label>
-                </div>
-                <div className="daily-details-grid-item">
-                  <label>Wind:</label>
-                  <label>{item.wind.speed} mph</label>
-                </div>
-                <div className="daily-details-grid-item">
-                  <label>Feels Like:</label>
-                  <label>{item.main.feels_like}°F</label>
-                </div>
-              </div>
-            </AccordionItemPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </div>
+          {/* AirQuality component */}
+          <div className="air-quality-info">
+            <AirQuality data={city} />
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
   );
 };
 
-export default Forecast;
+export default CurrentWeather;
